8510369f3143418113c51aa7d2a84299d5e9fbec
Markobanduka <banduka44@gmail.com>
2024-08-04 22:43:40 +0200

    added style to Task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import Login from "./components/Login";
 import Tasks from "./components/Tasks";
-import UserData from "./components/UserData";
 import { RecoilRoot } from "recoil";
 
 const App = () => {
   return (
     <>
       <RecoilRoot>
-        <Tasks />
-        <UserData />
         <Login />
+        <Tasks />
       </RecoilRoot>
     </>
   );
